476a89832066d6eb2657509258b1f7714d4a7d2b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-07 08:44:58 +0300

    Create 05-center-widget2.dart

---
+++ 05-center-widget2.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Icon(
+            Icons.calendar_month_outlined,
+            color: Colors.blue,
+            size: 250.0,
+          )
+        )
+      )
+    )
+  );
+}
+
